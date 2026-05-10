d42e4fed24eddc2b4c13c3e815a6a67cf5a17bfd
KiwiKing2k <dinustefan04@gmail.com>
2026-05-10 21:19:31 +0300

    initial project setup

---
+++ src/main/java/com/proiect/lostandauctioned/LostAndAuctionedApplication.java
@@ -0,0 +1,13 @@
+package com.proiect.lostandauctioned;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostAndAuctionedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LostAndAuctionedApplication.class, args);
+    }
+
+}
